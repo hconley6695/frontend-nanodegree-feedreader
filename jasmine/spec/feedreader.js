4f61c46502bc2b88a034cdfb00261472ccc4e20f
Heather Conley <hconley6695@gmail.com>
2017-11-02 21:57:09 -0400

    finished final test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -114,8 +114,34 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
+    describe("New Feed Selection", function(){
+
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         var initialLoadFeed;
+
+         beforeEach(function(done){
+            loadFeed(0, function(){
+                initialLoadFeed = document.querySelector('.feed').innerHTML;
+
+                loadFeed(1, function() {
+                    done();
+                });
+
+            });
+         });
+
+         it("has new feed if the content changes", function(done) {
+            var changedLoadFeed = document.querySelector('.feed').innerHTML;
+
+            expect(initialLoadFeed).not.toBe(changedLoadFeed);
+
+            done();
+         });
+
+    });
+
+
 }());
